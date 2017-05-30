8e27b670d9d770ad559f5405fa11cf203b7d74c2
Thomas Hurst <tom@hur.st>
2017-05-30 22:37:57 +0100

    License under MIT.

--- borg-backup.sh
+++ borg-backup.sh
@@ -2,10 +2,27 @@
 #
 # borg-backup.sh - BorgBackup shell script
 #
-# Author: Thomas Hurst <tom@hur.st>
+# Copyright (c) 2017 Thomas Hurst <tom@hur.st>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
-# No warranty expressed or implied. Beware of dog. Slippery when wet.
-# This isn't going to be your *only* backup method, right?
+###############################################################################
 #
 # Synopsis:
 #
